1e8b12dbbf556583d5b1689e03f47ae1f068e206
Freddy Polanía <fpolania@gmail.com>
2021-04-04 17:41:38 -0400

    23. Assigment 1: Practicing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManualComponent } from './manual/manual.component';
+import { AutoComponent } from './auto/auto.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ManualComponent, AutoComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
