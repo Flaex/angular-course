80e450b5f7d86510df4c8d45d1fcc965de426832
Freddy Polanía <fpolania@gmail.com>
2021-04-10 17:23:17 -0400

    Practicing Directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,12 +9,18 @@ export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   // function to get server status
   getServerStatus() {
     return this.serverStatus;
-  }
+  }  
 
-  constructor() {}
+  getColor() {
+    return this.serverStatus == 'online' ? 'green' : 'red';
+  }
 
   ngOnInit(): void {}
 }
